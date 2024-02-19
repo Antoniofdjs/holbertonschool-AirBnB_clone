cd2f45b3a8f977e4cb6f5c726df3c926df1b537b
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-19 18:51:04 -0400

    first version for base_model

--- models/base_model.py
+++ models/base_model.py
@@ -5,18 +5,31 @@
 
 import uuid
 import datetime
+
+
 class BaseModel:
 
     def __init__(self):
         id = uuid.uuid4()
-        date = datetime.datetime.now()
+
         self.id = str(id)
-        self.created_at = date.isoformat()
-        self.updated_at = date.isoformat()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
+    def to_dict(self):
+        """returns dictionary of keys/values of __dict__ of the instance:"""
+        dictionary = self.__dict__.copy()
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['__class__'] = self.__class__.__name__
+        return dictionary
 
-obj = BaseModel()
-print(obj.id)
-print(obj.created_at)
-print(obj.updated_at)
- # TESTTINGGGG
+    def save(self):
+        """save new update date"""
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """str representation of obj"""
+        class_name = self.__class__.__name__
+
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
