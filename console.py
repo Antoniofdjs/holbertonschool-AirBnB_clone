eb7ea722f3228b4d2ca0b811ccb1882773bcb1e3
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-22 11:36:49 -0400

    Add- def show, def destroy, def create

--- console.py
+++ console.py
@@ -6,6 +6,8 @@ Contains the entry point of the command interpreter
 """
 
 import cmd
+from models.base_model import BaseModel
+import json
 
 class HBNBCommand(cmd.Cmd):
 
@@ -22,6 +24,68 @@ class HBNBCommand(cmd.Cmd):
         """ Ctrl + D to exit the program"""
         print("")
         return True
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_model = BaseModel()
+            new_model.save()
+            print(f"{new_model.id}")
+
+    def do_show(self, arg):
+        """ Print string rprsnt of an instance based on the class and id"""
+        arg = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif arg[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
+        else:
+            try:
+                with open("file.json", 'r') as file:
+                    dict_from_json = json.load(file)
+                    name_id = arg[0] + "." + arg[1]
+                    if name_id in dict_from_json:
+                        obj_dict = dict_from_json[name_id]
+                        print(f"{BaseModel(**obj_dict)}")
+                    else:
+                        print("** no instance found **")
+            except FileNotFoundError:
+                print("** file not found **")
+    
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id """
+        arg = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif arg[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
+        else:
+            try:
+                with open("file.json", 'r') as file:
+                    dict_from_json = json.load(file)
+                    name_id = arg[0] + "." + arg[1]
+                    if name_id in dict_from_json:
+                        del dict_from_json[name_id]
+                    else:
+                        print("** no instance found **")
+            except FileNotFoundError:
+                print("** file not found **")
+
+            #  Reload the data back into dictionary
+            try:
+                with open("file.json", 'w') as file:
+                    json.dump(dict_from_json, file)
+            except FileNotFoundError:
+                print("** file not found **")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
