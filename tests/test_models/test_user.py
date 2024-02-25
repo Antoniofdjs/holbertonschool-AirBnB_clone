b40a363a0cde23bf2714f980197b4dca03591814
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-24 21:43:58 -0400

    removed-  import of basemodel

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,7 +6,6 @@
 '''
 import unittest
 from models.user import User
-from models.base_model import BaseModel
 
 
 class test_state(unittest.TestCase):
@@ -27,7 +26,6 @@ class test_state(unittest.TestCase):
     def test_id(self):
         u1 = User()
         u2 = User()
-        self.assertIsInstance(u1, BaseModel)
         self.assertTrue(hasattr(u1, "id"))
         self.assertNotEqual(u1.id, u2.id)
         self.assertIsInstance(u1.id, str)
