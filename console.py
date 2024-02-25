88ab01f5dd2f3df1296cacb03251d1644516fa99
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-25 00:30:06 -0400

    Removed place_dictionary from console, no use for it

--- console.py
+++ console.py
@@ -29,19 +29,6 @@ class HBNBCommand(cmd.Cmd):
         "Place": Place,
         "Review": Review
         }
-    __place_dict = {
-        "city_id": str,
-        "user_id": str,
-        "name": str,
-        "description": str,
-        "number_rooms": int,
-        "number_bathrooms": int,
-        "max_guest": int,
-        "price_by_night": int,
-        "latitude": float,
-        "longitude": float,
-        "amenity_ids": []
-    }
 
     def emptyline(self):
         pass
