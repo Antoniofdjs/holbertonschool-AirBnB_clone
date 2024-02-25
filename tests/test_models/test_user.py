4a93308ecc448ffac111f6c89f9e89fca81bd5bd
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-24 20:58:07 -0400

    Add- Unitest for User

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+
+    def test_stateid_name(self):
+        u = User()
+        u.email = "@test"
+        u.password= "123"
+        u.first_name = "ant"
+        u.last_name = "dejesus"
+        self.assertEqual(u.email, "@test")
+        self.assertEqual(u.password, "123")
+        self.assertEqual(u.first_name, "ant")
+        self.assertEqual(u.last_name, "dejesus")
+
+    def test_id(self):
+        u1 = User()
+        u2 = User()
+        self.assertIsInstance(u1, BaseModel)
+        self.assertTrue(hasattr(u1, "id"))
+        self.assertNotEqual(u1.id, u2.id)
+        self.assertIsInstance(u1.id, str)
+
+if __name__ == '__main__':
+
+    unittest.main()
