52ae8199834f0e22287c702cc25a4b3798b82c6b
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-26 10:43:18 -0400

    New version for unitest of User

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,33 +2,44 @@
 '''
     test_user.py
     
-    Unittest
+    Unittest for class city
 '''
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class test_user(unittest.TestCase):
     '''
         Class User Unittest
     '''
-    def test_user(self):
-        u = User()
-        u.email = "@test"
-        u.password= "123"
-        u.first_name = "ant"
-        u.last_name = "dejesus"
-        self.assertEqual(u.email, "@test")
-        self.assertEqual(u.password, "123")
-        self.assertEqual(u.first_name, "ant")
-        self.assertEqual(u.last_name, "dejesus")
 
-    def test_id(self):
-        u1 = User()
-        u2 = User()
-        self.assertTrue(hasattr(u1, "id"))
-        self.assertNotEqual(u1.id, u2.id)
-        self.assertIsInstance(u1.id, str)
+    def test_user(self):
+        '''
+            Class User test cases
+        '''
+
+        # Check if user has the expected attrs
+        user = User()
+        self.assertTrue(hasattr(user, 'email'))
+        self.assertTrue(hasattr(user, 'password'))
+        self.assertTrue(hasattr(user, 'first_name'))
+        self.assertTrue(hasattr(user, 'last_name'))
+
+        # Check if attrs are initialized to empty strs
+        self.assertEqual(user.email, '')
+        self.assertEqual(user.password, '')
+        self.assertEqual(user.first_name, '')
+        self.assertEqual(user.last_name, '')
+
+    def test_inheritance(self):
+        '''
+            Check if class inherits from BaseModel
+        '''
+        user = User()
+
+        # Check if User class inherits from BaseModel
+        self.assertTrue(issubclass(User, BaseModel))
 
 
 if __name__ == '__main__':
