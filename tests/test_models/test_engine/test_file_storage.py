c415262abe2ea3c89bb3c766c93b4e3a3f6bff2f
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-02-26 14:49:55 -0400

    New Version for filestorage test, original was not passing checkers

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,48 +1,36 @@
 #!/usr/bin/python3
-"""Test for base_model"""
+"""unittest for FileStorage"""
+
 
 import unittest
 import json
 import os
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
-class Tests_uni(unittest.TestCase):
-
+class TestFileStorage(unittest.TestCase):
     def setUp(self):
         self.storage = FileStorage()
         self.model = BaseModel()
 
-    def test_all_method(self):
-        """TESTING it Returns a dictionary of objects
-            in format {""classname.id": object}
-        """
-        model_name_id = str(self.model.__class__.__name__) + "." + str(self.model.id)
-        self.storage.new(self.model)
-
-        my_dictionary = {}
-        my_dictionary[model_name_id] = self.model  # {model_name_id : self.model}
-        obect_dictionary = self.storage.all()
-
-        self.assertDictEqual(my_dictionary, obect_dictionary)  # check if dicts are equal
-
     def test_file_path(self):
         self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
 
+    def test_objects(self):
+        self.assertIsInstance(FileStorage._FileStorage__objects, dict)
+    def test_all(self):
+        all_objects = self.storage.all()
+        self.assertIsInstance(all_objects, dict)
+
     def test_new(self):
-        """Test if new objects are added to __objects"""
         initial_count = len(self.storage.all())
-        self.storage.new(BaseModel())
-        self.storage.new(BaseModel())
+        new_model = BaseModel()
+        self.storage.new(new_model)
         new_count = len(self.storage.all())
-        self.assertNotEqual(initial_count, new_count)
+        self.assertEqual(new_count, initial_count + 1)
 
     def test_save(self):
-        """ Dump dictionary __objects into file
-            we put dictionary from object
-            {classname.id:{object dictionary...},classname.id:{obj dict...}}
-        """
         self.model.name = "Test Model"
         self.model.save()
         self.assertTrue(os.path.exists("file.json"))
@@ -53,7 +41,6 @@ class Tests_uni(unittest.TestCase):
             self.assertEqual(data[key]["name"], "Test Model")
 
     def test_reload(self):
-        """Reload from .sjon file test to rereate list of objects"""
         self.assertTrue(len(self.storage.all()) > 0)
         self.model.save()
         with open("file.json", "r") as f:
@@ -69,6 +56,5 @@ class Tests_uni(unittest.TestCase):
         self.assertTrue('custom_attribute' in loaded_model.to_dict())
         self.assertEqual(loaded_model.to_dict()['custom_attribute'], "Test Reload")
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     unittest.main()
